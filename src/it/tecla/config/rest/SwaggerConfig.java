00c08ac1c2fb945d41ab4e53cb90f2fecb580e5e
Nicolò Chieffo <n.chieffo@tecla.it>
2017-04-14 09:12:13 +0200

    aa

--- src/it/tecla/config/rest/SwaggerConfig.java
+++ src/it/tecla/config/rest/SwaggerConfig.java
@@ -8,14 +8,10 @@ import java.net.URLEncoder;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebListener;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({ "/swagger-ui/*", "/swagger/*" })
-@WebListener
 public class SwaggerConfig extends HttpServlet implements ServletContextListener {
 
 	private static final long serialVersionUID = 1L;
@@ -30,6 +26,7 @@ public class SwaggerConfig extends HttpServlet implements ServletContextListener
 		BeanConfig beanConfig = new BeanConfig();
 		beanConfig.setVersion("1.0");
 		beanConfig.setBasePath(contextPath + "/api");
+		// TODO ottimizzare
 		beanConfig.setResourcePackage("io.swagger.resources,it.tecla");
 		beanConfig.setScan(true);
 	}
@@ -42,7 +39,8 @@ public class SwaggerConfig extends HttpServlet implements ServletContextListener
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.sendRedirect(contextPath + "/resources/webjars/swagger-ui/3.0.5/?url=" + URLEncoder.encode(contextPath + "/api/swagger.json", "UTF-8"));
+		String swaggerUiVersion = this.getInitParameter("swagger-ui.version");
+		response.sendRedirect(contextPath + "/webjars/swagger-ui/" + swaggerUiVersion + "/?url=" + URLEncoder.encode(contextPath + "/api/swagger.json", "UTF-8"));
 		
 	}
 
